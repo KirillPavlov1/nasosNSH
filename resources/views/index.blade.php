5b31f4eca03c734c41a7570bb0e427dce7651a30
Kirill <pavlov140700@mail.ru>
2024-02-21 12:39:55 +0300

    21feb

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,22 +11,6 @@
 @section('content')
 <main class="mainMain">
     <section class="section__banner">
-        {{-- <div class="imgBg__wrapper">
-            <div class="img__bg">
-                <div class="container">
-                    <div class="banner__wrapper">
-                        <div class="nameAndDescription">
-                            <h3 class="h3Index">Насосы<br>шестеренные</h3>
-                            <div class="descriptionLines">
-                                <img src="images/lines.svg">
-                                <h4>гидравлические насосы НШ для различной<br>спецтехники выского качества </h4>
-                            </div>
-                        </div>
-                        <img src="images/pumps.svg" class="img__pumps">
-                    </div>
-                </div>
-            </div>
-        </div> --}}
        <!-- Slider main container -->
             <div class="swiper">
                 <!-- Additional required wrapper -->
